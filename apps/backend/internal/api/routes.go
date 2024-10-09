cff4a870e65bf11d13c6f3333766a9d473e6edec
mislav <mislavj@gmail.com>
2024-10-09 21:55:38 +0200

    remove duplicate routes

--- apps/backend/internal/api/routes.go
+++ apps/backend/internal/api/routes.go
@@ -14,7 +14,6 @@ func (api *Api) Routes() *httprouter.Router {
 	// chain := alice.New(api.corsMiddleware, api.authMiddleware)
 	chain := alice.New(api.corsMiddleware)
 	router.Handler(http.MethodGet, "/v1/test", chain.Then(http.HandlerFunc(api.testToken)))
-	router.HandlerFunc(http.MethodGet, "/v1/chat_bot", api.handleStream)
 	router.HandlerFunc(http.MethodGet, "/v1/data", api.handleGetTestData)
 	router.Handler(http.MethodGet, "/v1/chat_bot", chain.Then(http.HandlerFunc(api.handleStream)))
 	router.HandlerFunc(http.MethodGet, "/v1/speech_to_speech", api.handleWebSocket)
